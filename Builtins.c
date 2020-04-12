db1327382bb23002b963fcf9d5d7efd2a3898045
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-12 18:17:35 +0100

    command exit, cd, print env

---
+++ Builtins.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+/**
+ * cd - function
+ * @cmd: cmd
+ * Return: 0 for success
+ */
+int cd(char **cmd)
+{
+chdir(cmd[1]);
+return (0);
+}
+
+/**
+ * exitexe - function to exit
+ * @cmd: cmd
+ * Return: 0 for success
+ */
+int exitexe(char **cmd)
+{
+if (*cmd)
+exit(1);
+return (0);
+}
+
+/**
+ * printenv - function
+ * @cmd: pointer to cmd
+ * Return: 0 for success
+ */
+int printenv(char **cmd)
+{
+int i;
+
+if (*cmd)
+{
+i = 0;
+while (environ[i])
+{
+write(1, environ[i], strlen(environ[i]));
+write(1, "\n", 1);
+i++;
+}
+}
+return (0);
+}
+
+/**
+ * checkBuiltins - function
+ * @combine: full dir
+ * @cmd: cmd
+ * Return: path to builtin // process from directory
+ */
+int checkBuiltins(char *combine, char **cmd)
+{
+int i;
+int stats;
+char *array[] = {"cd", "exit", "env", NULL};
+
+typedef int (*Builtins)(char **);
+Builtins f[] = {&cd, &exitexe, &printenv};
+
+i = 0;
+while (array[i] != NULL)
+{
+if (strcmp(array[i], cmd[0]) == 0)
+return (f[i](cmd));
+i++;
+}
+stats = exe(combine, cmd);
+return (stats);
+}
