aa386ab85e105586bc3d4fa37301c0e7eb8b3fd3
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-15 21:30:07 +0100

    betty style

--- exe.c
+++ exe.c
@@ -6,18 +6,19 @@
  * @cmd: cmd input
  * Return: status
  */
+
 int exe(char *fullPath, char **cmd)
 {
-pid_t child;
-int status = 0;
+	pid_t child;
+	int status = 0;
 
-child = fork();
-if (child == 0)
-{
-status = execve(fullPath, cmd, NULL);
-exit(status);
-}
-else
-wait(NULL);
+	child = fork();
+		if (child == 0)
+		{
+			status = execve(fullPath, cmd, NULL);
+			exit(status);
+		}
+		else
+			wait(NULL);
 return (status);
 }
