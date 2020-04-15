471378e4adafeedeb1d35feed7e58c82fd1aad36
jlassi1 <khawlajlassi1990@gmail.com>
2020-04-15 21:38:22 +0100

    betty style

--- getcmd.c
+++ getcmd.c
@@ -4,18 +4,20 @@
  * get_line - function
  * Return: buffer
  */
+
 char *get_line(void)
 {
-char *buf = NULL;
-size_t bufsize = 0;
-int get;
+	char *buf = NULL;
+	size_t bufsize = 0;
+	int get;
 
-get = getline(&buf, &bufsize, stdin);
-if (get == EOF)
-{
-write(1, "\n", 1);
-free(buf);
-}
+	get = getline(&buf, &bufsize, stdin);
+
+	if (get == EOF)
+	{
+		write(1, "\n", 1);
+		free(buf);
+	}
 return (buf);
 }
 
@@ -24,31 +26,34 @@ return (buf);
  * @line: cmd input
  * Return: tokens
  */
+
 char **split_line(char *line)
 {
-char *dup = _strdup(line);
-char *token;
-char *t;
-char **tok = NULL;
-int i;
+	char *dup = _strdup(line);
+	char *token;
+	char *t;
+	char **tok = NULL;
+	int i;
 
 token = strtok(line, DELIM);
-while (token)
-{
-token = strtok(NULL, DELIM);
-i++;
-}
+
+	while (token)
+	{
+		token = strtok(NULL, DELIM);
+		i++;
+	}
+
 free(line);
 
 tok = malloc(sizeof(char *) * i + 1);
 t = strtok(dup, DELIM);
 i = 0;
-while (t)
-{
-tok[i] = t;
-t = strtok(NULL, DELIM);
-i++;
-}
-tok[i] = '\0';
+	while (t)
+	{
+		tok[i] = t;
+		t = strtok(NULL, DELIM);
+		i++;
+	}
+		tok[i] = '\0';
 return (tok);
 }
